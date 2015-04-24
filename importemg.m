452ae7192b8c09075724a02f7f76e7598773ad41
robby <robbystk@gmail.com>
2015-04-24 14:19:02 -0600

    removed time basis creation from import

--- importemg.m
+++ importemg.m
@@ -1,4 +1,4 @@
-function [ t delt tri starts stops ] = emgimport( filename )
+function [ delt tri starts stops ] = emgimport( filename )
 %emgimport: imports emg data from a csv file
 %   [ t delt tri starts stops ] = emgimport( filename )
 %   input:  the filename of the csv you wish to import
@@ -9,9 +9,6 @@ function [ t delt tri starts stops ] = emgimport( filename )
 %           list of indexes of 'start' event markers (third column)
 %           list of indexes of 'stop' markers (fourth)
 
-fs = 960;   % sample frequency
-ts = 1000/fs;  
-
 M = csvread(filename,1,0); % import whole csv
 
 % extract columns
@@ -20,12 +17,8 @@ tri   = M(:,2); % tricep data (second column)
 start = M(:,3); % start data (third column)
 stop  = M(:,4); % stop data (fourth column)
 
-% create time basis
-t = [0:ts:ts * (length(delt) - 1)]; % fucking off-by-one errors
-
 % extract start and stop breakpoints
 starts = find(start);
 stops  = find(stop);
 
 end
-
