e5c372c03fb1a0dfe65c92f63a4a2349f3b20af7
robby <robbystk@gmail.com>
2015-04-24 14:49:10 -0600

    created except (not sure why)

---
+++ excerpt.m
@@ -0,0 +1,22 @@
+function exerpt = exerpt(signal, starts, stops, n)
+%exerpt:    extracts a portion of a signal based on the nth pair of limits
+%           in starts and stops
+%   input:  the signal from which the excerp will be taken (signal)
+%           an array of start markers (starts)
+%           an array of stop markers (stops)
+%           the number of the event to be extracted (n)
+%   output: excerpt of the signal between the two markers
+
+if n > min(length(starts),length(stops))
+    disp("n is too big");
+    return
+end
+
+if starts(n) >= stops(n)
+    disp("start/stop mismatch");
+    return
+end
+
+excerpt = signal(starts(n):stops(n);
+
+end
