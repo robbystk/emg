f626bb700b8b6baa2b1a74990c4a9bd4814492ec
Robby <robbystk@rams.colostate.edu>
2015-04-25 14:30:15 -0600

    FUCK INDEXING FROM ONE WITH THE FURY OF A THOUSAND SUNS

--- movingRMS.m
+++ movingRMS.m
@@ -7,12 +7,14 @@ function [ RMS tRMS ] = movingRMS(signal, timebasis, n)
 %   output: array the RMS values of every n samples,
 %           array of the corresponding time for each RMS value
 
-lenRMS = floor(length(signal) / n);    % how many RMSs will be calculated
-starts = [0:n:n * lenRMS];      
-stops = [n:n:length(signal);
+lenRMS = floor(length(signal) / n) - 1; % how many RMSs will be calculated
+starts = 1:n:(n * lenRMS - 1);
+stops = n:n:length(signal);
 
 for i=1:lenRMS,
-    RMS(i) = rms(excerpt(signal, starts, stops, i);
+    RMS(i) = rms(excerpt(signal, starts, stops, i));
     tRMS(i) = timebasis(n * (i - 1) + 1);
     % ^ bojankety because MATLAB DOESN'T INDEX FROM ZERO >:|
+end % for
+
 end
