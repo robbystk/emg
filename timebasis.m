29d929fb0079f63b91807b2601d90b7a5d90f156
Robby <robbystk@rams.colostate.edu>
2015-04-27 21:35:21 -0600

    transpose time basis

--- timebasis.m
+++ timebasis.m
@@ -4,8 +4,8 @@ function t = timebasis(signal, fs)
 %   input:  the signal, its sample frequency fs
 %   output: array of t-values the same length as the sample
 
-ts = 1000/fs;   % sampling interval in ms
+ts = 1000/fs;       % sampling interval in ms
 tl = ts * (length(signal) - 1); % time of the last sample
-t = [0:ts:tl];  % time basis
+t = [0:ts:tl].';    % time basis
 
 end
