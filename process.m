996a6fbd49e8dd1866317abdb19e8fab407aba1d
Robby <robbystk@rams.colostate.edu>
2015-05-03 15:27:46 -0600

    made mostly work

--- process.m
+++ process.m
@@ -7,10 +7,17 @@ for subject = 1:5,
         else
             exstr = 'perfectpushup';
         end %if
-        filename = sprintf('../data/%s.sub%d.csv',exstr,subject)
-        [tri,delt] = importemg(filename,fs)
-        for rep = 1:5,
-            
-        end %for rep
+        filename = sprintf('../data/%s.sub%d.csv',exstr,subject);
+        [muscles(1),muscles(2)] = importemg(filename,fs);
+        for muscle = 1:2,
+            filtered = filteremg(deMainsEMG(muscles(muscle)));
+            for rep = 1:5,
+                part = crop(filtered,rep);
+                part.signal = detrend(part.signal);
+                trials(subject,exercise,rep,muscle) = part;
+                movingrms(subject,exercise,rep,muscle) = movingRMS(part,75);
+                totalrms(subject,exercise,rep,muscle) = rms(part.signal);
+            end %for rep
+        end %for muscle       
     end %for exercise
 end %for subject
