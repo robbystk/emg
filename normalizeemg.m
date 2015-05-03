64ac893510374942d43071e285c859461a093feb
Robby <robbystk@rams.colostate.edu>
2015-05-03 17:59:45 -0600

    removed now-unnecessary MVIC-related functions

--- normalizeemg.m
+++
@@ -1,9 +0,0 @@
-function normalized = normalizeemg(emg, mvic)
-%normalizeemg: normalizes an EMG signal to an MVIC measurement
-%   usage:  normalized = normalizeemg(emgsignal, mvic);
-%   input:  the signal to be normalized
-%           the value to normalize it to
-%   output: the normalized signal
-
-normalized = emg;
-normalized.signal = normalized.signal / mvic;
