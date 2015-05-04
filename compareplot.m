1531849be4ce60c1acaf90a107b1d50fbacb8dab
Robby <robbystk@rams.colostate.edu>
2015-05-04 16:27:28 -0600

    Created function to plot exercises side-by-side

---
+++ compareplot.m
@@ -0,0 +1,40 @@
+function compareplot(array, subject, drep, prep)
+%compareplot: plots a comparison between exercises and muscles
+%   usage:  compareplot(array, subject, rep);
+%   input:  the array to get the data from 
+%           the number of the subject
+%           the rep number to use for dumbbell press
+%           the rep number to use for perfect pushup
+%   output: none
+
+% get emgs out of array
+dpdelt  = array(subject,1,drep,1);
+dptri   = array(subject,1,drep,2);
+pudelt  = array(subject,2,prep,1);
+putri   = array(subject,2,prep,2);
+
+figure
+
+% plot Dumbbell Press
+subplot(2,1,1); hold on;
+plot(dpdelt.time,dpdelt.signal,'b-');
+plot(dptri.time,dptri.signal,'r-');
+ylim([0 2.5]);
+title('Dumbbell Press');
+xlabel('Time (ms)');
+ylabel('RMS EMG activity (mV)');
+legend('Deltoid','Tricep','Location','NorthWest');
+hold off;
+
+% plot Perfect Pushup
+subplot(2,1,2); hold on;
+plot(pudelt.time,pudelt.signal,'b-');
+plot(putri.time,putri.signal,'r-');
+ylim([0 2.5]);
+title('Perfect Pushup');
+xlabel('Time (ms)');
+ylabel('RMS EMG activity (mV)');
+legend('Deltoid','Tricep','Location','NorthWest');
+hold off;
+
+end %function
