950fd92b4a1951a1822ec0576b5b5306d3db2f60
Robby <robbystk@rams.colostate.edu>
2015-05-04 16:03:34 -0600

    created plot function that takes color

---
+++ colorplotemg.m
@@ -0,0 +1,20 @@
+function plotemg(emg,color)
+%plotemg:   Plots an EMG including start and stop events
+%   usage:  plotemg(emg);
+%   input:  EMG struct
+%   outupt: none
+
+plot(emg.time,emg.signal,color); hold on;
+if emg.n > 0
+    for i=emg.starts,
+        startt = emg.time(i);
+        plot([startt,startt],ylim,'g-');
+    end % for
+    for i=emg.stops,
+        stopt = emg.time(i);
+        plot([stopt,stopt],ylim,'r-');
+    end %for
+end %if
+hold off;
+
+end %function
